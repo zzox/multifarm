76149b3e7e7e57a2cd9b928f23e7e5dbd68aa8f7
zzox <ty@zzox.net>
2026-03-29 22:19:09 -0500

    smaller tiles

--- src/core/const.ts
+++ src/core/const.ts
@@ -3,8 +3,8 @@ export const Width = 240
 export const Height = 160
 
 // size of tiles
-export const TileWidth = 24
-export const TileHeight = 16
+export const TileWidth = 12
+export const TileHeight = 8
 
 // size of game in tiles
 export const NumTilesWidth = Width / TileWidth
